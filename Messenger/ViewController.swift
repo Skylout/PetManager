17d9be0f20c7bf4af52daa0bcfb6ab73b4c17234
Leonid Shapetko <Magicka@yandex.ru>
2020-07-31 15:27:04 -0700

    Initial Commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Леонид on 31.07.2020.
+//  Copyright © 2020 LSDevStudy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
